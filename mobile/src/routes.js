e914d6cc9631ca3a26ddfdffc1bddf2cf1a60eb4
farleybsd <farley.t.i@hotmail.com>
2020-01-25 20:18:18 -0300

    FINISH

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,5 +1,5 @@
-import {createAppContainer} from 'react-navigation'
-import {createStackNavigator} from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
 
 import Main from './pages/main'
 import Profile from './pages/Profile'
@@ -7,11 +7,28 @@ import Profile from './pages/Profile'
 const Routes = createAppContainer(
     createStackNavigator(
         {
-        Main,
-        Profile,
-        }
-        
-    )
+            Main: {
+                screen: Main,
+                navigationOptions: {
+                    title: 'DevRadar'
+                }
+            },
+            Profile: {
+                screen: Profile,
+                navigationOptions: {
+                    title: 'Perfil no GitHub'
+                }
+            }
+
+        },{
+            defaultNavigationOptions:{
+                headerTintColor:'#FFF',
+                headerBackTitleVisible:false,
+                headerStyle:{
+                    backgroundColor: '#7D40E7'
+                }
+            }
+        })
 )
 
 export default Routes
